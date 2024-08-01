a8c6989997b639316e3f9e8ce9b795a7fcc926c6
YvanBlanchette <yvanblanchette@gmail.com>
2024-08-01 13:18:56 -0400

    PWA configuration

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,6 @@
-//Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v2';
 
-
-//Add list of files to cache here.
+// Add list of files to cache here.
 const FILES_TO_CACHE = [
     'offline.html',
     'index.html',
@@ -37,13 +35,12 @@ self.addEventListener('install', (evt) => {
 
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
-    //Remove previous cached data from disk.
+    // Remove previous cached data from disk.
     evt.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if (key !== CACHE_NAME) {
-                    console.log('[ServiceWorker] Removing old cache',
-                        key);
+                    console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
             }));
@@ -52,21 +49,30 @@ self.addEventListener('activate', (evt) => {
     self.clients.claim();
 });
 
-
 self.addEventListener('fetch', (evt) => {
     console.log('[ServiceWorker] Fetch', evt.request.url);
-    //Add fetch event handler here.
     if (evt.request.mode !== 'navigate') {
-        // Not a page navigation, bail.
         return;
     }
     evt.respondWith(
         fetch(evt.request)
+            .then((response) => {
+                // If we received a valid response, return it.
+                if (response && response.status === 200 && response.type === 'basic') {
+                    return response;
+                }
+                // Otherwise, fetch the fallback page.
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('offline.html');
+                    });
+            })
             .catch(() => {
+                // Network request failed, try to get it from the cache.
                 return caches.open(CACHE_NAME)
                     .then((cache) => {
                         return cache.match('offline.html');
                     });
             })
     );
-})
+});
